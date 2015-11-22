5b6ae9961d0bd5d013b87f04323929d7f7a929d2
JackDesBwa <jackdesbwa@gmail.com>
2015-11-22 12:27:42 +0100

    hex2bin: rewrite loop to be less hacky

--- hex2bin.c
+++ hex2bin.c
@@ -7,23 +7,19 @@ int main() {
 	size_t addr = 0;
 	int iRead = 0;
 	while((iRead = read(0, data, sizeof(data) - 1)) > 0) {
-		int hex, iScanned;
+		int hex, iScanned, ok;
 		char * pdata = data, * pdata_old;
 		data[iRead] = 0; // Add a null character at end of read data
-		while (1) {
-			static i;
-			i++;
-			if (i==20) break;
+		do {
 			pdata_old = pdata;
 			hex = strtol(pdata, &pdata, 16);
-			if (pdata != pdata_old) {
+			ok = (pdata != pdataold && pdata < (char*) data + rd);
+			if (ok) {
 				unsigned char uchhex = hex;
 				printf("%c", uchhex);
 				if (uchhex != hex) fprintf(stderr, "%x truncated to %x\n", hex, uchhex);
-			} else {
-				break;
 			}
-		}
+		} while (ok);
 		fflush(stdout);
 	}
 	return 0;
